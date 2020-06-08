2c1804e0731cd3d373d69e35bc6882955f39fb8b
dansh0 <dshores16@gmail.com>
2020-06-08 18:50:00 -0400

    server for local use and updates

--- src/ThreeTest.js
+++ src/ThreeTest.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@v0.108.0/build/three.module.js";
-import {OrbitControls} from "https://cdn.jsdelivr.net/npm/three@v0.108.0/examples/jsm/controls/OrbitControls.js";
+// import {OrbitControls} from "https://cdn.jsdelivr.net/npm/three@v0.108.0/examples/jsm/controls/OrbitControls.js";
 
 var container;
 var camera, scene, raycaster, renderer;
