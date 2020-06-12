1d213f459badd718af78f52427528beb991a035f
dansh0 <dshores16@gmail.com>
2020-06-12 16:35:57 -0400

    added objLoader, having trouble with texture mapping now

--- src/scene.js
+++ src/scene.js
@@ -1,5 +1,7 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@v0.108.0/build/three.module.js";
-import {OrbitControls} from "https://cdn.jsdelivr.net/npm/three@v0.108.0/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@v0.108.0/examples/jsm/controls/OrbitControls.js";
+import { STLLoader } from "./modules/STLLoader.js";
+import { OBJLoader } from "./modules/OBJLoader.js";
 
 // scene
 var scene = new THREE.Scene();
@@ -7,8 +9,8 @@ var scene = new THREE.Scene();
 // camera
 let canvasWidth = window.innerWidth;
 let canvasHeight = window.innerHeight;
-let multiplier = 200;
-var camera = new THREE.OrthographicCamera(canvasWidth/-multiplier,canvasWidth/multiplier,canvasHeight/multiplier,canvasHeight/-multiplier,0.254,50)
+let multiplier = 10;
+var camera = new THREE.OrthographicCamera(canvasWidth/-multiplier,canvasWidth/multiplier,canvasHeight/multiplier,canvasHeight/-multiplier,-50,50)
 
 // renderer
 var renderer = new THREE.WebGLRenderer();
@@ -28,17 +30,17 @@ const loadManager = new THREE.LoadingManager();
 const loader = new THREE.TextureLoader(loadManager);
 
 let faces = {
-	black: new THREE.MeshPhongMaterial({map: loader.load( "src/resources/textures/textures_black.png" )}),
-	blue: new THREE.MeshPhongMaterial({map: loader.load( "src/resources/textures/textures_blue.png" )}),
-	green: new THREE.MeshPhongMaterial({map: loader.load( "src/resources/textures/textures_green.png" )}),
-	orange: new THREE.MeshPhongMaterial({map: loader.load( "src/resources/textures/textures_orange.png" )}),
-	red: new THREE.MeshPhongMaterial({map: loader.load( "src/resources/textures/textures_red.png" )}),
-	white: new THREE.MeshPhongMaterial({map: loader.load( "src/resources/textures/textures_white.png" )}),
-	yellow: new THREE.MeshPhongMaterial({map: loader.load( "src/resources/textures/textures_yellow.png" )})
+	black: new THREE.MeshPhongMaterial({color: 0x000000}),//{map: loader.load( "src/resources/textures/textures_black.png" )}),
+	blue: new THREE.MeshPhongMaterial({color: 0x0000ff}),//{map: loader.load( "src/resources/textures/textures_blue.png" )}),
+	green: new THREE.MeshPhongMaterial({color: 0x00ff00}),//{map: loader.load( "src/resources/textures/textures_green.png" )}),
+	orange: new THREE.MeshPhongMaterial({color: 0xff5555}),//{map: loader.load( "src/resources/textures/textures_orange.png" )}),
+	red: new THREE.MeshPhongMaterial({color: 0xff0000}),//{map: loader.load( "src/resources/textures/textures_red.png" )}),
+	white: new THREE.MeshPhongMaterial({color: 0xffffff}),//{map: loader.load( "src/resources/textures/textures_white.png" )}),
+	yellow: new THREE.MeshPhongMaterial({color: 0x00ffff})//{map: loader.load( "src/resources/textures/textures_yellow.png" )})
 }
 
 Object.keys(faces).forEach(mat => {
-	faces[mat].map.anisotropy = 16;
+	// faces[mat].map.anisotropy = 16;
 	faces[mat].shininess = 100;
 	// faces[mat].color = 0x996633;
     // faces[mat].specular = 0x050505;
@@ -59,47 +61,80 @@ let zPosColor = [faces.black, faces.black, faces.yellow]
 
 // prepare geometry
 let cubes = []
-var geometry = new THREE.BoxGeometry();
+// var geometry = new THREE.BoxGeometry();
+// var stlLoader = new STLLoader(loadManager);
+var objLoader = new OBJLoader(loadManager);
+// let geometry = stlLoader.load('src/resources/models/cube.stl')
 
-// generate mesh and add to scene
-loadManager.onLoad = () => {
+objLoader.load( 'src/resources/models/cube.obj', function ( object ) {
+	// var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200, wireframe: false } );
+	// var mesh = new THREE.Mesh( geometry, material );
+	// mesh.position.set( -1.5, -0.7, -0.7 );
+	// //mesh.rotation.set( 0, - Math.PI / 2, 0 );
+	// mesh.scale.set( 0.5, 0.5, 0.5 );
+	// mesh.castShadow = true;
+	// mesh.receiveShadow = true;
+	// scene.add( mesh );
 
-	// let matArray = [faces.green, faces.yellow, faces.white, faces.blue, faces.orange, faces.red]
+	console.log(object)
 
-	xPos.forEach(i => {
-		yPos.forEach(j => {
-			zPos.forEach(k => {
+	// generate mesh and add to scene
+	loadManager.onLoad = () => {
 
-				// set face definition
-				let matArray = [
-					xPosColor[i],
-					xNegColor[i],
-					yPosColor[j],
-					yNegColor[j],
-					zPosColor[k],
-					zNegColor[k]
-				]
+		var material = new THREE.MeshPhongMaterial( { color: 0xff5533, specular: 0x111111, shininess: 200, wireframe: false } );
+		// var meshSTL = new THREE.Mesh( geometry, material );
+		// meshSTL.position.set( -1.5, -0.7, -0.7 );
+		// //meshSTL.rotation.set( 0, - Math.PI / 2, 0 );
+		// meshSTL.scale.set( 0.5, 0.5, 0.5 );
+		// meshSTL.castShadow = true;
+		// meshSTL.receiveShadow = true;
+		// scene.add( meshSTL );
+		// console.log(meshSTL)
 
-				let index = i*3+j*3+k
-				let mesh = new THREE.Mesh( geometry, matArray );
-				let cube = {
-					mesh: mesh,
-					material: faces.blue,
-					xPos: 1.05*(i-1),
-					yPos: 1.05*(j-1),
-					zPos: 1.05*(k-1),
-					// color: colorArray[index],
-					index: index
-				}
-				console.log(cube)
-				cube.mesh.translateX(cube.xPos)
-				cube.mesh.translateY(cube.yPos)
-				cube.mesh.translateZ(cube.zPos)
-				scene.add( cube.mesh );
+		// let matArray = [faces.green, faces.yellow, faces.white, faces.blue, faces.orange, faces.red]
+
+		xPos.forEach(i => {
+			yPos.forEach(j => {
+				zPos.forEach(k => {
+
+					// set face definition
+					let matArray = [
+						xPosColor[i],
+						xNegColor[i],
+						yPosColor[j],
+						yNegColor[j],
+						zPosColor[k],
+						zNegColor[k]
+					]
+
+					let index = i*3+j*3+k
+					let mesh = object.clone();
+					mesh.traverse( function( child ) {
+			            if ( child instanceof THREE.Mesh ) {
+			                child.material = matArray[index%6];
+			            }
+			        } );
+					// let mesh = new THREE.Mesh( geometry, faces.red); //matArray );
+					let cube = {
+						mesh: mesh,
+						material: faces.blue,
+						xPos: 19.05*(i-1),
+						yPos: 19.05*(j-1),
+						zPos: 19.05*(k-1),
+						// color: colorArray[index],
+						index: index
+					}
+					console.log(cube)
+					cube.mesh.translateX(cube.xPos)
+					cube.mesh.translateY(cube.yPos)
+					cube.mesh.translateZ(cube.zPos)
+					scene.add( cube.mesh );
+				})
 			})
 		})
-	})
-}
+	}
+} );
+
 
 // lighting
 var light = new THREE.PointLight( 0xffffff, 10, 100 );
